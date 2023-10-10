bc38e37b8484ca72ed532120d0a91eef13db0831
202296 <mouhamedkoko996@gmail.com>
2023-10-10 09:14:58 +0000

    remove unccessary files

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -10,10 +10,6 @@ const {
     logout, 
     loginAdmin,
     saveAddress,
-    // createOrder,
-    // getOrders,
-    // updateOrderStatus,
-    // getAllOrders,
 } = require('../controllers/userController');
 const { authMiddleware, isAdmin } = require('../middlewares/authMiddleware');
 
@@ -23,22 +19,11 @@ const router = express.Router();
 router.post('/register', createUser);
 router.post('/login', loginUserCtrl);
 router.post("/admin-login", loginAdmin);
-// router.post("/cart/cash-order", authMiddleware, createOrder);
 router.get('/all-users', getallUser);
-// router.get("/get-orders", authMiddleware, getOrders);
-// router.get("/getallorders", authMiddleware, isAdmin, getAllOrders);
-// router.post("/getorderbyuser/:id", authMiddleware, isAdmin, getAllOrders);
-
 router.get('/refresh', handleRefreshToken);
 router.get('/logout', logout);
 router.get('/:id', authMiddleware , isAdmin, getaUser);
 router.delete('/:id', deleteaUser);
-router.put(
-    "/order/update-order/:id",
-    authMiddleware,
-    isAdmin,
-    // updateOrderStatus
-  );
 router.put('/edit-user', authMiddleware, UpdateaUser);
 router.put("/save-address", authMiddleware, saveAddress);
 
